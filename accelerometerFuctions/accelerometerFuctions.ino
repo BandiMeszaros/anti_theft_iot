a8d57b2354f77d0587961c1de485b7de122c7754
Andras <attila.andras.meszaros@gmail.com>
2022-01-18 13:28:33 +0100

    The unit is possible to send alarm signals now.
    It is not yet a library.

--- accelerometerFuctions/accelerometerFuctions.ino
+++ accelerometerFuctions/accelerometerFuctions.ino
@@ -7,19 +7,18 @@
  * analog 25: z-axis
  * analog 24: y-axis
  * analog 23: x-axis
- * test
- 
- 
+
+
  Absolute rating/conversion can be determined from the ADXL3xx datasheet.
- As a quick reference, for LaunchPads with 12-bit ADC (MSP430F5529, Tiva C, etc.), 
- the entire analog  range is [0,4096]. For simple tilting calculation 
+ As a quick reference, for LaunchPads with 12-bit ADC (MSP430F5529, Tiva C, etc.),
+ the entire analog  range is [0,4096]. For simple tilting calculation
  [-1g,1g] ~ = [mid-800, mid + 800] = [2048-800,2048+800] = [1248,2848]
 
 */
-#include<stdbool.h>  
-#define DEBUGMODE true
 
-const int value_threshold = 200;
+#define DEBUGMODE 0
+
+const int value_threshold = 700;
 const int xpin = 23; // x-axis of the accelerometer
 const int ypin = 24; // y-axis
 const int zpin = 25; // z-axis (only on 3-axis models)
@@ -29,29 +28,69 @@ const int blueLED= 37; // assign blueLED to Pin #12
 static int xpin_rest = 0;
 static int ypin_rest = 0;
 static int zpin_rest = 0;
+static int measureResults[3];
+static int diffResults[3];
+static int movmentFlag = 0;
+const int pushbuttonBooster1 = 32;
+const int pushbuttonBooster2 = 33;
 
+void alarm()
+{
+  //someone is stealing your shit
+  if(movmentFlag)
+  {
+    digitalWrite(redLED, LOW);
+    digitalWrite(greenLED, LOW);
+    digitalWrite(blueLED, LOW);
+    Serial.print("\nALARM!!!, someone is stealing your shit, call the police!!!\n");
+
+    //this is just a demo, if other modules are ready it can be deleted
+    while(1)
+    {
+      Serial.print("ALARM!!!!!!\n");
+      digitalWrite(redLED,HIGH);
+      delay(250);
+      digitalWrite(redLED, LOW);
+      delay(250);
+    }
+  }
+}
 void warning_movment()
 {
   //changes the LED color green-->red
   //sends a warning message to the computer
   digitalWrite(redLED, HIGH);
   digitalWrite(greenLED, LOW);
-  Serial.print("\nWARNING!!!, someone is stealing your shit\n");
-  delay(1000);
+  Serial.print("\nWARNING, WARNING, movement detected\n");
+  movmentFlag = 1;
+}
+
+void printDiffData(int diffX, int diffY, int diffZ)
+{
+  Serial.print(" -------------\n");
+  Serial.print("Xdiff: ");
+  Serial.print(diffX);
+  Serial.print("\n");
+  Serial.print("Ydiff: "); // print to serial monitor
+  Serial.print(diffY);
+  Serial.print("\n");
+  Serial.print("Zdiff: "); // print to serial monitor
+  Serial.print(diffZ);
+  Serial.print("\n");
 }
 void printAccelerometerData(int xpinV, int ypinV, int zpinV)
 {
 
   Serial.print(" -------------\n");
-  Serial.print("RED (X): "); 
-  Serial.print(xpinV); 
-  Serial.print("\n");   
-  Serial.print("GREEN (Y): "); // print to serial monitor 
-  Serial.print(ypinV); 
+  Serial.print("RED (X): ");
+  Serial.print(xpinV);
   Serial.print("\n");
-  Serial.print("BLUE (Z): "); // print to serial monitor 
-  Serial.print(zpinV); 
-  Serial.print("\n"); 
+  Serial.print("GREEN (Y): "); // print to serial monitor
+  Serial.print(ypinV);
+  Serial.print("\n");
+  Serial.print("BLUE (Z): "); // print to serial monitor
+  Serial.print(zpinV);
+  Serial.print("\n");
 }
 
 void printRestValues()
@@ -73,56 +112,73 @@ void printRestValues()
     Serial.print("----------");
 }
 
-int * measureAnalogValues()
+void measureAnalogValues()
 {
   //Measures the analogValues of the x,y,z axis, and returns them in an array
 
-  int analogValueX;// CHECK X AXIS (red) 
-  analogValueX = analogRead(xpin);// read X axis 
+  int analogValueX;// CHECK X AXIS (red)
+  analogValueX = analogRead(xpin);// read X axis
   int analogValueY;
   analogValueY = analogRead(ypin); //read y axis
   int analogValueZ;
   analogValueZ = analogRead(zpin); // read Z axis
-
-  // todo: array cannot be returned this way
-  int results[3] = {analogValueX, analogValueY, analogValueZ};
+  measureResults[0] = analogValueX;
+  measureResults[1] = analogValueY;
+  measureResults[2] = analogValueZ;
 
   if(DEBUGMODE)
   {
     Serial.print("These are the measured values:\n");
     printAccelerometerData(analogValueX,analogValueY,analogValueZ);
-    delay(5000);
+    delay(1000);
   }
-  
-  return results;
 }
 
-int * calculateChange(int analogVX, int analogVY, int analogVZ)
+void calculateChange(int analogVX, int analogVY, int analogVZ)
 {
   //calculates the difference between the measured x,y,z values, and the resting position values
-    
+
     int xDiff = abs(analogVX-xpin_rest);
     int yDiff = abs(analogVY-ypin_rest);
     int zDiff = abs(analogVZ-zpin_rest);
 
-    //todo array cannot be returned this way
-    int results[3] = {xDiff, yDiff, zDiff};
+    if(DEBUGMODE)
+    {
+      Serial.print("\nThese are the values after substraction:\n");
+      printDiffData(xDiff, yDiff, zDiff);
+      Serial.print("And these are the measrued values and the rest values:\n\n\n");
+      printAccelerometerData(analogVX, analogVY, analogVZ);
+      printRestValues();
+    }
 
-    return results;
+    diffResults[0] = xDiff;
+    diffResults[1] = yDiff;
+    diffResults[2] = zDiff;
 }
 
-bool movementDetected(int axisDiffs[])
+int movementDetected(int axisDiffs[])
 {
   //it checks if the detected accelerometer value is in the threshold
 
   for(int i=0; i<3;i++)
   {
-    if (axisDiffs[i]-value_threshold)
+    if (0 > value_threshold-axisDiffs[i])
      {
-        return true;
+       if(DEBUGMODE)
+       {
+         Serial.print("\n\n---------\nmovement detector thinks that this is the threshlod-axisDiffs[i] value:\t---->\t");
+         Serial.print(value_threshold-axisDiffs[i]);
+         Serial.print("\n");
+       }
+        return 1;
      }
+     Serial.print("\n\n---------\nmovement detector thinks that this is the threshlod-axisDiffs[i] value:\t---->\t");
+     Serial.print(value_threshold-axisDiffs[i]);
+     Serial.print("\nwhere i is:");
+     Serial.print(i);
+     Serial.print("\n");
   }
-  return false;
+  return 0;
 }
 
 void calculateRestValues()
@@ -147,42 +203,85 @@ void calculateRestValues()
     zpin_rest = zpin_sum/loopCounter;
 }
 void setup() {
-    // By default MSP432 has analogRead() set to 10 bits. 
+    // By default MSP432 has analogRead() set to 10 bits.
     // to 12 bit resolution for MSP432.
     analogReadResolution(12);
     Serial.begin(9600); // initialize for sending diagnostic info to computer
     pinMode(greenLED, OUTPUT);
     pinMode(redLED, OUTPUT);
+    pinMode(blueLED, OUTPUT);
+    pinMode(pushbuttonBooster1, INPUT);
+    pinMode(pushbuttonBooster2, INPUT);
     digitalWrite(redLED, HIGH);
-    
+
     calculateRestValues();
+
 
     digitalWrite(redLED, LOW);
     digitalWrite(greenLED, HIGH);
+    digitalWrite(blueLED, LOW);
 
     if(DEBUGMODE == true)
     { //this code only runs if the DEBUGMODE constanst is true
     printRestValues();
-    
     }
-  
+
 }
 
 void loop() {
 
-    int* measuredValues;
-    measuredValues = measureAnalogValues();
-     
+    measureAnalogValues();
+    int analogValueX = measureResults[0];
+    int analogValueY = measureResults[1];
+    int analogValueZ = measureResults[2];
+    calculateChange(analogValueX,analogValueY,analogValueZ);
+
+    //for DEBUGMODE
+    int diffX = diffResults[0];
+    int diffY = diffResults[1];
+    int diffZ = diffResults[2];
+
+    int moving = movementDetected(diffResults);
+    if(moving)
+    {
+      warning_movment();
+    }
+    if(movmentFlag)
+    {
+      int timer = 0;
+      int button2 = 0;
+      int button3 = 0;
+      for(timer; timer<2000; timer++)
+      {//5000 loops until it alarms the police
+        button2 = digitalRead(pushbuttonBooster1);
+        button3 = digitalRead(pushbuttonBooster2);
+        Serial.print("\nTimer value:  ");
+        Serial.print(timer);
+        if(DEBUGMODE)
+        {
+          Serial.print("\n This is the values of the buttons, during warning phase:\n");
+          Serial.print(button2);
+          Serial.print("\n");
+          Serial.print(button3);
+          Serial.print("\n");
+        }
+        if (!button2 && !button3)
+        {
+          //disalarm
+          movmentFlag = 0;
+          digitalWrite(blueLED, HIGH); //drive mode
+          digitalWrite(redLED, LOW);
+          break;
+        }
+      }
+      alarm();
+    }
+
     if (DEBUGMODE == true)
     //this code only runs if the DEBUGMODE constanst is true
     {
       delay(1000);
-
-      //todo there is a little problem with this, fix it the array cannot be returned the way it is done here 
-      int analogValueX = measuredValues[0];
-      int analogValueY = measuredValues[1];
-      int analogValueZ = measuredValues[2];
       printAccelerometerData(analogValueX, analogValueY, analogValueZ);
-      
+      printDiffData(diffX, diffY, diffZ);
     }
 }
